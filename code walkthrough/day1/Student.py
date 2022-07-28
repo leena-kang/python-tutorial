f1dd9132db9f8024be054dfd222736258cf2b69a
Leena Kang <leenak100@gmail.com>
2022-07-28 00:56:40 -0700

    gir acad vid

--- code walkthrough/day1/Student.py
+++ code walkthrough/day1/Student.py
@@ -1,8 +1,15 @@
  #creating a student class 
 class Student: 
     
-    def __init__(self, name, major, gpa, is_on_probation): 
+    def __init__(self, name, major, gpa): 
         self.name = name 
         self.major = major 
         self.gpa = gpa
-        self.is_on_probation = is_on_probation
+
+# can also put functions in class
+#for object func, self has to be first parameter
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else: 
+            return False
--- code walkthrough/day1/classes&object2.py
+++ code walkthrough/day1/classes&object2.py
@@ -1,8 +1,8 @@
 from Student import Student
 #from *file name* import *class func name* 
 
-student1 = Student("Jim", "Business", 3.1, False)
-student2 = Student("Pam", "Math", 3.5, True)
+student1 = Student("Jim", "Business", 3.1)
+student2 = Student("Pam", "Math", 3.5)
 
 print(student1.name)
 print(student2.gpa)
